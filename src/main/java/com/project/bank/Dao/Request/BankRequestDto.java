69d38ec226af8ab78f8eae44e8ab713691a76079
Lucas Uhrich <lucasuhrich318@gmail.com>
2023-07-31 11:42:14 -0300

    Añadida dependencia para validacion de datos

--- src/main/java/com/project/bank/Dao/Request/BankRequestDto.java
+++ src/main/java/com/project/bank/Dao/Request/BankRequestDto.java
@@ -3,6 +3,7 @@ package com.project.bank.Dao.Request;
 import java.io.Serializable;
 
 public class BankRequestDto implements Serializable {
+
     private final String name;
 
     public BankRequestDto(String name) {
